400ce5085ae45f95cf360b8524ad6207a6d2d2d7
ilyes14hat <67545986+ilyes14hat@users.noreply.github.com>
2022-02-24 21:17:01 +0100

    Delete hashcode22-main directory

--- hashcode22-main/da.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from collections import Counter
-
-
-def stats(base_classes):
-    return
--- hashcode22-main/practice/strategy.py
+++
@@ -1,8 +0,0 @@
-from da import count_ing
-
-
-def no_haters(clients):
-    likes, hates = count_ing(clients)
-    likes = set(likes.keys())
-    hates = set(hates.keys())
-    return likes.difference(hates)
